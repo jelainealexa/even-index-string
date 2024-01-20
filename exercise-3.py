ad78613cea487aad0f619b1b0a6ceaef9554c4ff
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-20 20:06:15 +0800

    Print word

--- exercise-3.py
+++ exercise-3.py
@@ -1,11 +1,13 @@
 # Print characters from a string that are present at an even index number
 
 # Declare the word variable
+word = "polytechnic"
 
 # Iterate each character of the string
 
 # Start: 
 # Stop: 
 # Step: 
-
-# Print characters at the even index number
+for i in range(11):
+    # Print characters at the even index number
+    print(word[i])
