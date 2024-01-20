a714da3dcddcf1d6f47762ebd7c58ca892918ac5
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-20 20:01:57 +0800

    Pseudocodes

---
+++ exercise-3.py
@@ -0,0 +1,11 @@
+# Print characters from a string that are present at an even index number
+
+# Declare the word variable
+
+# Iterate each character of the string
+
+# Start: 
+# Stop: 
+# Step: 
+
+# Print characters at the even index number
